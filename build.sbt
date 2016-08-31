780e98153c3edf999a091a01153cd368d77b5126
Nicolas A Perez <anicolaspp@gmail.com>
2016-08-31 15:45:26 -0400

    init

---
+++ build.sbt
@@ -0,0 +1,26 @@
+
+name := "twl"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+
+libraryDependencies ++= Seq(
+  "com.github.finagle" %% "finch-core" % "0.11.0-M2",
+  "com.github.finagle" %% "finch-circe" % "0.11.0-M2",
+  "com.github.finagle" %% "finch-test" % "0.11.0-M2",
+
+  "io.circe" %% "circe-core" % "0.4.1",
+  "io.circe" %% "circe-generic" % "0.4.1",
+  "io.circe" %% "circe-parser" % "0.4.1"
+
+)
+libraryDependencies += "com.twilio.sdk" % "twilio-java-sdk" % "5.10.1"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.3.0"
+
+enablePlugins(JavaAppPackaging)
---
+++ src/main/scala/com/nico/twl/app.scala
@@ -0,0 +1,80 @@
+/**
+  * Created by nperez on 8/31/16.
+  */
+
+
+package com.nico.twl
+
+import com.twilio.sdk.TwilioRestClient
+import com.twitter.finagle.Http
+import com.twitter.util.{FuturePool, Await}
+import io.finch._
+import io.finch.circe._
+import io.circe.generic.auto._
+import org.apache.http.NameValuePair
+import org.apache.http.message.BasicNameValuePair
+
+import scala.collection.JavaConverters._
+
+
+case class Message(to: String, body: String)
+
+case class MessageList(items: List[Message])
+
+object app {
+
+  def main(args: Array[String]) {
+
+    val port = Option(System.getProperty("http.port")) getOrElse "9090"
+
+    val accountid = System.getenv("ACCOUNT_ID")
+
+    val authToken = System.getenv("AUTH_TOKEN")
+
+    val sendOne: Endpoint[String] = post("sms" :: "one" :: body.as[Message] :: headerExists("token")) {
+      (msg: Message, t: String) =>
+        val p: List[NameValuePair] = List(
+          new BasicNameValuePair("From", "+13053631921"),
+          new BasicNameValuePair("To", msg.to),
+          new BasicNameValuePair("Body", msg.body)
+        )
+
+        val client = new TwilioRestClient(accountid, authToken)
+        val messageFactory = client.getAccount().getMessageFactory
+        val message = messageFactory.create(p.asJava)
+
+        println(message.getSid)
+        println(message.getStatus)
+
+        Ok("OK")
+    }
+
+    val sendMany: Endpoint[String] = post("sms" :: "many" :: body.as[MessageList]
+      :: headerExists("token").should("be valid") { _ == "lolotoken"}) {
+      (messages: MessageList, t: String) =>
+        println(t)
+        FuturePool.unboundedPool {
+          messages.items.foreach { msg =>
+
+            val p: List[NameValuePair] = List(
+              new BasicNameValuePair("From", "+13053631921"),
+              new BasicNameValuePair("To", msg.to),
+              new BasicNameValuePair("Body", msg.body)
+            )
+            val client = new TwilioRestClient(accountid, authToken)
+            val messageFactory = client.getAccount().getMessageFactory
+            val message = messageFactory.create(p.asJava)
+
+            println(message.getSid)
+            println(message.getStatus)
+          }
+
+          Ok("OK")
+        }
+    }
+
+    val api = sendMany :+: sendOne
+
+    Await.ready(Http.server.serve(s":$port", api.toService))
+  }
+}
